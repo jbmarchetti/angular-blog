7ceb0b2b584dae7d9d50099064a642afe35081a4
Jean Baptiste <marchetti.jb@gmail.com>
2015-10-07 14:45:29 +0200

    test

--- src/blog.js
+++ src/blog.js
@@ -5,6 +5,16 @@ angular
 .config([
   '$stateProvider',
   function($stateProvider) {
+
+
+    var scripts = document.getElementsByTagName("script");
+   var currentScriptPath = scripts[scripts.length-1].src;
+   console.log("currentScriptPath:"+currentScriptPath);
+   var baseUrl = currentScriptPath.substring(0, currentScriptPath.lastIndexOf('/') + 1);
+   console.log("QCRIPTS baseUrl:"+baseUrl);
+
+
+
     $stateProvider
     .state('main.blog', {
       //abstract:true,
@@ -15,7 +25,7 @@ angular
     .state('main.blog.index', {
       //abstract:true,
       controller:   'BlogIndexCtrl',
-      templateUrl:  'index.html',
+      templateUrl:  baseUrl+'index.html',
       url:          ''
     })
     ;
